ff29b5e037445f81c04380b1b8c3e0c40a499aa4
Kaixin Bai <kaixin.bai@agile-robots.com>
2020-06-03 13:22:49 +0200

    .

--- INSTALL.md
+++ INSTALL.md
@@ -1,1 +1,19 @@
+# PVN3D安装记录
+
+## conan激活cuda和cudnn
+eval $(arpm_env cuda/10.0.130@ar/stable cudnn/7.6.0@ar/stable)
+
+## 创建conda虚拟环境
 conda create -n pvn3d python=3.6
+
+## 安装特定版本的pytorch
+conda install pytorch==1.0.1 torchvision==0.2.2 cudatoolkit=10.0 -c pytorch
+
+## build pn++
+cd /data/hdd1/kb/agile/pvn3d_test/PVN3D
+python3 setup.py build_ext
+
+## 下载linemod数据集的checkpoints
+https://hkustconnect-my.sharepoint.com/personal/yhebk_connect_ust_hk/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fyhebk_connect_ust_hk%2FDocuments%2Fpublically shared 共享文件夹%2FPVN3D_pretrained_model%2FLineMOD&originalPath=aHR0cHM6Ly9oa3VzdGNvbm5lY3QtbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwveWhlYmtfY29ubmVjdF91c3RfaGsvRWhheTVwUHRsLUJHbnZBRWNsWEJSdThCRXBaZUh1Rzl4LWFOX2RqQXR0NXJQQT9ydGltZT1kX21Rc2EwSDJFZw
+
+# 测试
--- pvn3d/datasets/ycb/ycb_dataset.py
+++ pvn3d/datasets/ycb/ycb_dataset.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import os
 import cv2
-import pcl
+# import pcl
+import open3d as o3d
 import torch
 import os.path
 import numpy as np
@@ -13,7 +14,6 @@ from lib.utils.basic_utils import Basic_Utils
 import scipy.io as scio
 import scipy.misc
 from cv2 import imshow, waitKey
-
 
 config = Config(dataset_name='ycb')
 bs_utils = Basic_Utils(config)
@@ -79,7 +79,7 @@ class YCB_Dataset():
         return item
 
     def rand_range(self, rng, lo, hi):
-        return rng.rand()*(hi-lo)+lo
+        return rng.rand() * (hi - lo) + lo
 
     def gaussian_noise(self, rng, img, sigma):
         """add gaussian noise of given sigma to image"""
@@ -111,7 +111,7 @@ class YCB_Dataset():
         # apply HSV augmentor
         if rng.rand() > 0:
             hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype(np.uint16)
-            hsv_img[:, : ,1] = hsv_img[:, :, 1] * self.rand_range(rng, 1.25, 1.45)
+            hsv_img[:, :, 1] = hsv_img[:, :, 1] * self.rand_range(rng, 1.25, 1.45)
             hsv_img[:, :, 2] = hsv_img[:, :, 2] * self.rand_range(rng, 1.15, 1.35)
             hsv_img[:, :, 1] = np.clip(hsv_img[:, :, 1], 0, 255)
             hsv_img[:, :, 2] = np.clip(hsv_img[:, :, 2], 0, 255)
@@ -144,51 +144,58 @@ class YCB_Dataset():
 
         return np.clip(img, 0, 255).astype(np.uint8)
 
+    # def get_normal(self, cld):
+    #     cloud = pcl.PointCloud()
+    #     cld = cld.astype(np.float32)
+    #     cloud.from_array(cld)
+    #     ne = cloud.make_NormalEstimation()
+    #     kdtree = cloud.make_kdtree()
+    #     ne.set_SearchMethod(kdtree)
+    #     ne.set_KSearch(50)
+    #     n = ne.compute()
+    #     n = n.to_array()
+    #     return n
     def get_normal(self, cld):
-        cloud = pcl.PointCloud()
         cld = cld.astype(np.float32)
-        cloud.from_array(cld)
-        ne = cloud.make_NormalEstimation()
-        kdtree = cloud.make_kdtree()
-        ne.set_SearchMethod(kdtree)
-        ne.set_KSearch(50)
-        n = ne.compute()
-        n = n.to_array()
+        cloud = o3d.geometry.PointCloud()
+        cloud.points = o3d.utility.Vector3dVector(cld)
+        cloud.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=50))
+        n = np.array(cloud.normals)
         return n
 
     def add_real_back(self, rgb, labels, dpt, dpt_msk):
         real_item = self.real_gen()
-        with Image.open(os.path.join(self.root, real_item+'-depth.png')) as di:
+        with Image.open(os.path.join(self.root, real_item + '-depth.png')) as di:
             real_dpt = np.array(di)
-        with Image.open(os.path.join(self.root, real_item+'-label.png')) as li:
+        with Image.open(os.path.join(self.root, real_item + '-label.png')) as li:
             bk_label = np.array(li)
         bk_label = (bk_label <= 0).astype(rgb.dtype)
         bk_label_3c = np.repeat(bk_label[:, :, None], 3, 2)
-        with Image.open(os.path.join(self.root, real_item+'-color.png')) as ri:
+        with Image.open(os.path.join(self.root, real_item + '-color.png')) as ri:
             back = np.array(ri)[:, :, :3] * bk_label_3c
         dpt_back = real_dpt.astype(np.float32) * bk_label.astype(np.float32)
 
         msk_back = (labels <= 0).astype(rgb.dtype)
         msk_back = np.repeat(msk_back[:, :, None], 3, 2)
-        rgb = rgb * (msk_back==0).astype(rgb.dtype) + back * msk_back
+        rgb = rgb * (msk_back == 0).astype(rgb.dtype) + back * msk_back
 
         dpt = dpt * (dpt_msk > 0).astype(dpt.dtype) + \
-            dpt_back * (dpt_msk <=0).astype(dpt.dtype)
+              dpt_back * (dpt_msk <= 0).astype(dpt.dtype)
         return rgb, dpt
 
     def get_item(self, item_name):
         try:
-            with Image.open(os.path.join(self.root, item_name+'-depth.png')) as di:
+            with Image.open(os.path.join(self.root, item_name + '-depth.png')) as di:
                 dpt = np.array(di)
-            with Image.open(os.path.join(self.root, item_name+'-label.png')) as li:
+            with Image.open(os.path.join(self.root, item_name + '-label.png')) as li:
                 labels = np.array(li)
-            meta = scio.loadmat(os.path.join(self.root, item_name+'-meta.mat'))
+            meta = scio.loadmat(os.path.join(self.root, item_name + '-meta.mat'))
             if item_name[:8] != 'data_syn' and int(item_name[5:9]) >= 60:
                 K = config.intrinsic_matrix['ycb_K2']
             else:
                 K = config.intrinsic_matrix['ycb_K1']
 
-            with Image.open(os.path.join(self.root, item_name+'-color.png')) as ri:
+            with Image.open(os.path.join(self.root, item_name + '-color.png')) as ri:
                 if self.add_noise:
                     ri = self.trancolor(ri)
                 rgb = np.array(ri)[:, :, :3]
@@ -206,7 +213,7 @@ class YCB_Dataset():
             dpt = bs_utils.fill_missing(dpt, cam_scale, 1)
             msk_dp = dpt > 1e-6
 
-            rgb = np.transpose(rgb, (2, 0, 1)) # hwc2chw
+            rgb = np.transpose(rgb, (2, 0, 1))  # hwc2chw
             cld, choose = bs_utils.dpt_2_cld(dpt, cam_scale, K)
             normal = self.get_normal(cld)[:, :3]
             normal[np.isnan(normal)] = 0.0
@@ -230,7 +237,7 @@ class YCB_Dataset():
                 np.random.shuffle(c_mask)
                 choose_2 = choose_2[c_mask.nonzero()]
             else:
-                choose_2 = np.pad(choose_2, (0, config.n_sample_points-len(choose_2)), 'wrap')
+                choose_2 = np.pad(choose_2, (0, config.n_sample_points - len(choose_2)), 'wrap')
 
             cld_rgb_nrm = np.concatenate((cld, rgb_pt, normal), axis=1)
             cld = cld[choose_2, :]
@@ -251,13 +258,13 @@ class YCB_Dataset():
                 RT = np.concatenate((r, t), axis=1)
                 RTs[i] = RT
 
-                ctr = bs_utils.get_ctr(self.cls_lst[cls_id-1]).copy()[:, None]
+                ctr = bs_utils.get_ctr(self.cls_lst[cls_id - 1]).copy()[:, None]
                 ctr = np.dot(ctr.T, r.T) + t[:, 0]
                 ctr3ds[i, :] = ctr[0]
                 msk_idx = np.where(labels == cls_id)[0]
 
-                target_offset = np.array(np.add(cld, -1.0*ctr3ds[i, :]))
-                ctr_targ_ofst[msk_idx,:] = target_offset[msk_idx, :]
+                target_offset = np.array(np.add(cld, -1.0 * ctr3ds[i, :]))
+                ctr_targ_ofst[msk_idx, :] = target_offset[msk_idx, :]
                 cls_ids[i, :] = np.array([cls_id])
 
                 key_kpts = ''
@@ -266,47 +273,46 @@ class YCB_Dataset():
                 else:
                     kp_type = 'farthest{}'.format(config.n_keypoints)
                 kps = bs_utils.get_kps(
-                    self.cls_lst[cls_id-1], kp_type=kp_type, ds_type='ycb'
+                    self.cls_lst[cls_id - 1], kp_type=kp_type, ds_type='ycb'
                 ).copy()
                 kps = np.dot(kps, r.T) + t[:, 0]
                 kp3ds[i] = kps
 
                 target = []
                 for kp in kps:
-                    target.append(np.add(cld, -1.0*kp))
+                    target.append(np.add(cld, -1.0 * kp))
                 target_offset = np.array(target).transpose(1, 0, 2)  # [npts, nkps, c]
                 kp_targ_ofst[msk_idx, :, :] = target_offset[msk_idx, :, :]
 
             # rgb, pcld, cld_rgb_nrm, choose, kp_targ_ofst, ctr_targ_ofst, cls_ids, RTs, labels, kp_3ds, ctr_3ds
             if DEBUG:
-                return  torch.from_numpy(rgb.astype(np.float32)), \
-                        torch.from_numpy(cld.astype(np.float32)), \
-                        torch.from_numpy(cld_rgb_nrm.astype(np.float32)), \
-                        torch.LongTensor(choose.astype(np.int32)), \
-                        torch.from_numpy(kp_targ_ofst.astype(np.float32)), \
-                        torch.from_numpy(ctr_targ_ofst.astype(np.float32)), \
-                        torch.LongTensor(cls_ids.astype(np.int32)), \
-                        torch.from_numpy(RTs.astype(np.float32)), \
-                        torch.LongTensor(labels.astype(np.int32)), \
-                        torch.from_numpy(kp3ds.astype(np.float32)), \
-                        torch.from_numpy(ctr3ds.astype(np.float32)), \
-                        torch.from_numpy(K.astype(np.float32)), \
-                        torch.from_numpy(np.array(cam_scale).astype(np.float32))
+                return torch.from_numpy(rgb.astype(np.float32)), \
+                       torch.from_numpy(cld.astype(np.float32)), \
+                       torch.from_numpy(cld_rgb_nrm.astype(np.float32)), \
+                       torch.LongTensor(choose.astype(np.int32)), \
+                       torch.from_numpy(kp_targ_ofst.astype(np.float32)), \
+                       torch.from_numpy(ctr_targ_ofst.astype(np.float32)), \
+                       torch.LongTensor(cls_ids.astype(np.int32)), \
+                       torch.from_numpy(RTs.astype(np.float32)), \
+                       torch.LongTensor(labels.astype(np.int32)), \
+                       torch.from_numpy(kp3ds.astype(np.float32)), \
+                       torch.from_numpy(ctr3ds.astype(np.float32)), \
+                       torch.from_numpy(K.astype(np.float32)), \
+                       torch.from_numpy(np.array(cam_scale).astype(np.float32))
 
-            return  torch.from_numpy(rgb.astype(np.float32)), \
-                    torch.from_numpy(cld.astype(np.float32)), \
-                    torch.from_numpy(cld_rgb_nrm.astype(np.float32)), \
-                    torch.LongTensor(choose.astype(np.int32)), \
-                    torch.from_numpy(kp_targ_ofst.astype(np.float32)), \
-                    torch.from_numpy(ctr_targ_ofst.astype(np.float32)), \
-                    torch.LongTensor(cls_ids.astype(np.int32)), \
-                    torch.from_numpy(RTs.astype(np.float32)), \
-                    torch.LongTensor(labels.astype(np.int32)), \
-                    torch.from_numpy(kp3ds.astype(np.float32)), \
-                    torch.from_numpy(ctr3ds.astype(np.float32)),
+            return torch.from_numpy(rgb.astype(np.float32)), \
+                   torch.from_numpy(cld.astype(np.float32)), \
+                   torch.from_numpy(cld_rgb_nrm.astype(np.float32)), \
+                   torch.LongTensor(choose.astype(np.int32)), \
+                   torch.from_numpy(kp_targ_ofst.astype(np.float32)), \
+                   torch.from_numpy(ctr_targ_ofst.astype(np.float32)), \
+                   torch.LongTensor(cls_ids.astype(np.int32)), \
+                   torch.from_numpy(RTs.astype(np.float32)), \
+                   torch.LongTensor(labels.astype(np.int32)), \
+                   torch.from_numpy(kp3ds.astype(np.float32)), \
+                   torch.from_numpy(ctr3ds.astype(np.float32)),
         except:
             return None
-
 
     def __len__(self):
         return len(self.all_lst)
@@ -344,21 +350,21 @@ def main():
     while True:
         # for cat in ['val', 'test']:
         for cat in ['test']:
-        # for cat in ['train']:
+            # for cat in ['train']:
             datum = ds[cat].__getitem__(idx[cat])
             idx[cat] += 1
             datum = [item.numpy() for item in datum]
             if cat == "train":
                 rgb, pcld, cld_rgb_nrm, choose, kp_targ_ofst, \
-                    ctr_targ_ofst, cls_ids, RTs, labels, kp3ds, ctr3ds, K, cam_scale = datum
+                ctr_targ_ofst, cls_ids, RTs, labels, kp3ds, ctr3ds, K, cam_scale = datum
             else:
                 rgb, pcld, cld_rgb_nrm, choose, kp_targ_ofst, \
-                    ctr_targ_ofst, cls_ids, RTs, labels, kp3ds, ctr3ds = datum
+                ctr_targ_ofst, cls_ids, RTs, labels, kp3ds, ctr3ds = datum
                 K = config.intrinsic_matrix['ycb_K1']
                 cam_scale = 1.0
             nrm_map = bs_utils.get_normal_map(cld_rgb_nrm[:, 6:], choose[0])
             imshow('nrm_map', nrm_map)
-            rgb = rgb.transpose(1, 2, 0)[...,::-1].copy()# [...,::-1].copy()
+            rgb = rgb.transpose(1, 2, 0)[..., ::-1].copy()  # [...,::-1].copy()
             for i in range(22):
                 p2ds = bs_utils.project_p3d(pcld, cam_scale, K)
                 # rgb = bs_utils.draw_p2ds(rgb, p2ds)
